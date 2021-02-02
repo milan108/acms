3f9e8313fcf0bb6f8a7c0c37066e01c68078cf8f
Milan <milan.lastname@outlook.com>
2021-02-01 23:02:42 -0500

    init

---
+++ src/test/java/com/miljkovic/acms/AcmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miljkovic.acms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
